c66a7ff75af39c55d25075993aee58d62f3583c8
Ziynatillo <zmorzugrand@gmail.com>
2024-04-03 11:51:38 +0500

    Fixed insert function

--- server/lib/insertDb.js
+++ server/lib/insertDb.js
@@ -3,15 +3,18 @@ const {Message }=require('../models/message')
  function addMessageDb(data){
     Message.countDocuments().then(async count=>{
         if(count>=50){
-            Message.findOneAndDelete({}, {sort:{createDateTime:-1}},async(err, deleteDoc)=>{
-                if(err){
-                    console.error(err)
-                }else{
-                    console.log('Oxirgi hujjat oxhirildi. '+deleteDoc)
-                    // Yangi hujat qo'shamiz
-                    const message = new Message(data)
-                    await message.save()
-                }
+            Message.findOneAndDelete({}, {sort:{createDateTime:-1}}).then(()=>{
+                async(err, deleteDoc)=>{
+                    if(err){
+                        console.error(err)
+                    }else{
+                        console.log('Oxirgi hujjat oxhirildi. '+deleteDoc)
+                        // Yangi hujat qo'shamiz
+                        const message = new Message(data)
+                        await message.save()
+                    }
+            }}).catch(err=>{
+                console.log(err)
             })
         }else{
            // Yangi hujat qo'shamiz
